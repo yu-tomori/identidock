f95f44d3355e9aa30ac0452ff607a1af667678a0
yu <peacetomori1118@gmail.com>
2020-11-06 22:05:21 +0900

    add logspout container and logstash container

--- identiproxy/entrypoint.sh
+++ identiproxy/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# Template対象のファイルとsedコマンドにおいて
+# {{}}で変数を囲うというルールを設けることで
+# 意図しない変換が行われないようにする
+# $~~は環境変数へのアクセス
+# コンテナの環境変数はdocker composeファイルで設定している
 sed -i "s|{{NGINX_HOST}}|$NGINX_HOST|;s|{{NGINX_PROXY}}|$NGINX_PROXY|" \
     /etc/nginx/conf.d/default.conf
 cat /etc/nginx/conf.d/default.conf
